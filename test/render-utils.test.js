87b1d95abba566f13a1cee51bb238b1101cb6550
David Francisco <dfrancisco2677@gmail.com>
2022-05-09 16:37:18 -0700

    A period.

--- test/render-utils.test.js
+++ test/render-utils.test.js
@@ -8,7 +8,8 @@ const test = QUnit.test;
 test('renderListItem renders a div with animal details', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected =  "<div style=\"top: 225px; left: 20%;\" class=\"animal\"><a href=\"./animals/?id=6\"><img src=\"../assets/sheep.svg\"><span class=\"name\">Samuel the Sheep</span><span>Just because we're sheep, it doesn't mean we have to follow. Think for yourselves.</span></a></div>";
+    const expected = `<div class="animal" style="top: 225px; left: 20%;"><a href="./animals/?id=6"><img src="../assets/sheep.svg"><span class="name">Samuel the Sheep</span><span>Just because we're sheep, it doesn't mean we have to follow. Think for yourselves.</span></a></div>`;
+
     //Act
     // Call the function you're testing and set the result to a const
     const samuel = {
